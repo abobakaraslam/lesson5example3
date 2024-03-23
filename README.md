c608f15cfc41d59c62990991274ed4cd95a2c971
abobakaraslam <m.abobakar.aslam@gmail.com>
2024-03-24 00:42:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson5example3
+ Example 3 in lesson as "Basic Example codes for tailwind css in react"
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//File: App.jsx
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <h1 className="text-red-700 text-center my-3 text-2xl">Author name</h1>
+      </div>
+      <div className="text-center w-[400px]">
+        <p>
+          This is introduction about the author. This introduction is written in
+          paragraph element. The author is interested in programming languages
+          related to web, mobile application. The author has expertise in
+          JavaScript and Python language.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
